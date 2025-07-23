6130912f44b464b4a6f62396c3c8b0e601e277bc
Megg <finero91@gmail.com>
2025-07-23 10:23:19 +0900

    temp commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-"use client";
-
-import { supabase } from "../../utils/client";
-import { useEffect } from "react";
+// import { supabase } from "../../utils/client";
+// import { useEffect } from "react";
 
 export default function Home() {
-  const getData = async () => {
-    const { data: test } = await supabase.from("test").select("*");
-    console.log(test);
-  };
+  // const getData = async () => {
+  //   const { data: test } = await supabase.from("test").select("*");
+  //   console.log(test);
+  // };
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div>
-      <button onClick={getData}>Get Data</button>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
     </div>
   );
