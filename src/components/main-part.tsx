12769ee40ab5efe9ba3f7ab9ef94fcccf7bf4661
최준호 <wnsgh1a@naver.com>
2025-07-23 13:54:34 +0900

    항목추가 버튼 기능

--- src/components/main-part.tsx
+++ src/components/main-part.tsx
@@ -2,16 +2,21 @@ import React from "react";
 
 type MainPartComponentProps = {
   children: React.ReactNode;
+  onAddItem: () => void;
 };
 
 export default function MainPartComponent({
   children,
+  onAddItem,
 }: MainPartComponentProps) {
   return (
     <div className="flex flex-col w-full h-fit items-center bg-[#ffd90066] p-10 gap-5">
       {children}
       <div className="w-[768px] flex justify-between">
-        <button className="bg-amber-300 w-20 h-8 rounded-xl text-white">
+        <button
+          className="bg-amber-300 w-20 h-8 rounded-xl text-white"
+          onClick={onAddItem}
+        >
           항목추가
         </button>
         <button className="bg-amber-300 w-20 h-8 rounded-xl text-white">
