c6030c0c971b93f3450c484ff79f9b0d187d52de
최준호 <wnsgh1a@naver.com>
2025-07-24 17:56:48 +0900

    0724

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,25 @@
 
 import React, { useState } from "react";
 
-// import { supabase } from "../../utils/client";
-// import { useEffect } from "react";
+import { supabase } from "../../utils/client";
+import { useEffect } from "react";
 import MainPartComponent from "../components/main-part";
 import MainTitleComponent from "../components/main-title";
 import QuestionBoxComponent from "../components/question-box";
 
 export default function Home() {
-  // const getData = async () => {
-  //   const { data: test } = await supabase.from("test").select("*");
-  //   console.log(test);
-  // };
+  const getData = async () => {
+    const { data: test } = await supabase.from("test").select("*");
+    console.log("2", test);
+  };
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
   const [questionBoxes, setQuestionBoxes] = useState([0]);
   const handleAddItem = () => {
     setQuestionBoxes((prevBoxes) => [...prevBoxes, prevBoxes.length]);
   };
 
-  return (
-    <div>
-      <MainPartComponent onAddItem={handleAddItem}>
-        <MainTitleComponent />
-        {questionBoxes.map((_, index) => (
-          <QuestionBoxComponent key={index} />
-        ))}
-      </MainPartComponent>
-    </div>
-  );
+  return <div className="min-w-100 min-h-100 bg-[#ffd90066]"></div>;
 }
